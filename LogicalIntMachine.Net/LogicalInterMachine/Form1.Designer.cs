36c8b92c4677bd404e6717265965a0c838fed6cc
Татьяна Андреева <tanyandree@gmail.com>
2020-10-24 23:41:39 +0500

    Form

--- LogicalIntMachine.Net/LogicalInterMachine/Form1.Designer.cs
+++ LogicalIntMachine.Net/LogicalInterMachine/Form1.Designer.cs
@@ -28,22 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(500, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(304, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Question:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Answer options:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Final result:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.ClientSize = new System.Drawing.Size(567, 209);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Travel choise";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- LogicalIntMachine.Net/LogicalInterMachine/Form1.cs
+++ LogicalIntMachine.Net/LogicalInterMachine/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using LogicalInterfaceMachine;
+using LogicalInterMachine.InterfaceMachine;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,10 +23,69 @@ namespace LogicalInterMachine
             logicalMachine = new LogicalMachine();
             logicalMachine.LoadKnowladgeBasw();
             logicalMachine.GetCurentRuler();
+            textBox1.Text = logicalMachine.CurQuestion;
+            if (logicalMachine.AnswersWays.Count>1)
+            {
+                for (int i=0; i< logicalMachine.AnswersWays.Count;i++)
+                {
+                    comboBox1.Items.Add(logicalMachine.AnswersWays[i]);
+                }
+            }
+            else
+            {
+                comboBox1.Items.Add("yes");
+                comboBox1.Items.Add("no");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (logicalMachine.ResaltAnswer != null)
+            {
+                textBox2.Text = logicalMachine.ResaltAnswer;
+            }
+            //else
+            //{
+                
+                bool otvet = false;
+
+                if (comboBox1.SelectedItem.ToString() == "yes" || comboBox1.SelectedItem.ToString() == "no")
+                {
+                    if (comboBox1.SelectedItem.ToString() == "yes") otvet = true;
+                    if (comboBox1.SelectedItem.ToString() == "no") otvet = false;
+                    UserAnswer userAnswer = new UserAnswer(otvet);
+                    logicalMachine.AddDataFromUser(userAnswer);
+                }
+                else
+                {
+                    UserAnswer userAnswer = new UserAnswer(comboBox1.SelectedItem.ToString());
+                    logicalMachine.AddDataFromUser(userAnswer);
+                }
+
+                logicalMachine.GetCurentRuler();
+                textBox1.Text = logicalMachine.CurQuestion;
+                comboBox1.Items.Clear();
+
+                if (logicalMachine.AnswersWays.Count > 1)
+                {
+                    for (int i = 0; i < logicalMachine.AnswersWays.Count; i++)
+                    {
+                        comboBox1.Items.Add(logicalMachine.AnswersWays[i]);
+                    }
+                }
+                else
+                {
+                    comboBox1.Items.Add("yes");
+                    comboBox1.Items.Add("no");
+                }
+            //}
 
         }
     }
