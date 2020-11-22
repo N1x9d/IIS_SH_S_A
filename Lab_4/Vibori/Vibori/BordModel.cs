bed938ee3a96d855bd5c5c0a70d943ee3e6cc980
MitiaSaihanov <71186341+MitiaSaihanov@users.noreply.github.com>
2020-11-22 20:29:34 +0500

    BorderModel

---
+++ Lab_4/Vibori/Vibori/BordModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vibori
+{
+    class BordModel
+    {
+        List<int[]> voitersResalts;
+        //колличество экспертов
+        int expert;
+        //Колличество оценок
+        int mark;
+        public BordModel(List<int[]> VoitersResalts)
+        {
+            voitersResalts = VoitersResalts;
+            expert = voitersResalts.Count;
+            mark = voitersResalts[0].Length;
+        }
+
+        public int CalculateResalt()
+        {
+            var markExpert = new int[mark];
+            for(int indexpert = 0; indexpert < expert; indexpert++)
+            {
+                for(int indmark=0; indmark <mark; indmark++)
+                {
+                    //Определяет index и отнимает от него колличесвто элементов -1 для выставления оценки и загоняет
+                    // в массив оценок
+                    markExpert[voitersResalts[indexpert][indmark] - 1] += Math.Abs(indmark - (mark - 1));
+                }
+            }
+            int maxmark=markExpert.Max<int>();
+            int resualt = Array.IndexOf(markExpert, maxmark);
+            return resualt + 1;
+        }
+    }
+}
--- Lab_4/Vibori/Vibori/Program.cs
+++ Lab_4/Vibori/Vibori/Program.cs
@@ -10,6 +10,31 @@ namespace Vibori
     {
         static void Main(string[] args)
         {
+            int VoitersCount = 3;
+            int AlternativesCount = 3;
+            var VoitersResalts = new List<int[]>();
+            Console.WriteLine("Введите прироитеты голосующих");
+            int i = 0;
+            while (i < VoitersCount)
+            {
+                int j = 0;
+                Console.WriteLine($"Введите приоритеты {i} избирателя");
+                int[] resalt = new int[AlternativesCount];
+                while (j < AlternativesCount)
+                {
+                    resalt[j] = Convert.ToInt32(Console.ReadLine());
+                    j++;
+                }
+                VoitersResalts.Add(resalt);
+                i++;
+
+            }
+            BordModel bordModel = new BordModel(VoitersResalts);
+            bordModel.CalculateResalt();
+        }
+        public void SetVoitersProirites()
+        {
+            
         }
     }
 }
