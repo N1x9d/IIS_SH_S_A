44ba9280e1458de503c14376ca7561510e926a6c
Татьяна Андреева <tanyandree@gmail.com>
2020-11-23 02:50:10 +0500

    Simpson Rule

--- Lab_4/Vibori/Vibori/Program.cs
+++ Lab_4/Vibori/Vibori/Program.cs
@@ -10,7 +10,7 @@ namespace Vibori
     {
         static void Main(string[] args)
         {
-            int VoitersCount = 3;
+            int VoitersCount =3;
             int AlternativesCount = 3;
             var VoitersResalts = new List<int[]>();
             Console.WriteLine("Введите прироитеты голосующих");
@@ -30,13 +30,21 @@ namespace Vibori
 
             }
             ConstituencyPriorites cp = new ConstituencyPriorites(AlternativesCount, VoitersCount, VoitersResalts);
+            Console.WriteLine();
             Console.WriteLine("Kondarse metod");
             cp.CalculateResaltByKondarse();
+            Console.WriteLine();
             Console.WriteLine("Kopland metod");
             cp.CalculateResaltByKopland();
+            Console.WriteLine();
             Console.WriteLine("BordModel");
             BordModel bordModel = new BordModel(VoitersResalts);
             bordModel.CalculateResalt();
+            Console.WriteLine();
+            Console.WriteLine("Simpson rule");
+            SimpsonRule sr = new SimpsonRule(VoitersResalts);
+            sr.CalculateResalt();
+            Console.ReadLine();
         }
         public void SetVoitersProirites()
         {
