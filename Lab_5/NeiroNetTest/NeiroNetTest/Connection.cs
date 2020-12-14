a8d0f5df599f69531fbbb4bf05b67778fb15e011
Никита Шаталов <nsh19021999@yandex.ru>
2020-12-14 21:06:21 +0500

    Добавлена породия на нейросеть

---
+++ Lab_5/NeiroNetTest/NeiroNetTest/Connection.cs
@@ -0,0 +1,18 @@
+﻿namespace NeiroNetTest
+{
+    public struct Connection
+    {
+        public int MatrixX => _matrixX;
+
+        public int MatrixY => _matrixY;
+
+        private int _matrixX;
+        private int _matrixY;
+
+        public Connection(int matrixX, int matrixY)
+        {
+            _matrixX = matrixX;
+            _matrixY = matrixY;
+        }
+    }
+}
---
+++ Lab_5/NeiroNetTest/NeiroNetTest/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace NeiroNetTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
